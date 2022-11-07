f08cfd94a86233fc0d711a642d830164855a68d3
hossein <shsamiei27ap@gmail.com>
2022-11-07 09:22:21 +0330

    product_detail completed

--- store/views.py
+++ store/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
+from rest_framework.response import Response 
+from .models import Product
+from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
 # django way : 
 # def product_list(request):
 #     return HttpResponse('ok')
@@ -12,8 +13,12 @@ from rest_framework.response import Response
 def product_list(request):
     return Response('ok')
 
+
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    # product = Product.objects.get(pk=id)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
 
