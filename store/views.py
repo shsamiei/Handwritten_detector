59590c120e02a1b79faac7f947b1e528a436b6c8
hossein <shsamiei27ap@gmail.com>
2022-11-07 08:15:57 +0330

    product_list add to views

---
+++ store/views.py
@@ -0,0 +1,13 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# django way : 
+# def product_list(request):
+#     return HttpResponse('ok')
+
+# rest way : 
+
+@api_view()
+def product_list(request):
+    return Response('ok')
